8d009bb4bd3e18541683a74ead279dcd5dbf9a51
Void-swap <136692389+Void-swap@users.noreply.github.com>
2023-11-27 00:51:15 +0530

    validParentheses.java

--- validParentheses.java
+++ validParentheses.java
@@ -1,40 +1,32 @@
 import java.util.*;
 
-public class validParentheses {
+public class hp {
     public static void main(String arg[]) {
-        String pare = "{[]}{}";
+        String pare = "{()[]}()";
 
         System.out.print(parenthesisChecker(pare));
-
     }
 
     public static boolean parenthesisChecker(String str) {
-
         Stack<Character> stk = new Stack<>();
 
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
 
-            if ((ch == '{') || (ch == '[') || (ch == '(')) {
+            if (ch == '{' || ch == '[' || ch == '(') {
                 stk.push(ch);
-            }
-
-            if ((ch == '}') && (stk.peek() == '{')) {
+            } else if (stk.isEmpty()) {
+                // If the stack is empty return
+                return false;
+            } else if (ch == '}' && stk.peek() == '{') {
+                stk.pop();
+            } else if (ch == ']' && stk.peek() == '[') {
+                stk.pop();
+            } else if (ch == ')' && stk.peek() == '(') {
                 stk.pop();
             }
-
-            if ((ch == ']') && (stk.peek() == '[')) {
-                stk.pop();
-            }
-
-            if ((ch == ')') && (stk.peek() == '(')) {
-                stk.pop();
-            }
-
         }
-
+        //still is stk empty or not
         return stk.isEmpty();
-
     }
-
 }
